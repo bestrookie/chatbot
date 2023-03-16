ade70e01d63c40c28921ffefb49ba87bc039e4f3
bestrookie <491051587@qq.com>
2023-03-16 20:43:40 +0800

    fix:添加依赖

--- chatbot-api-interfaces/src/main/java/com/bestrookie/Application.java
+++ chatbot-api-interfaces/src/main/java/com/bestrookie/Application.java
@@ -1,9 +1,17 @@
 package com.bestrookie;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author bestrookie
  * @Date 2023/3/16 20:24
  * @Desc 启动类
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+        System.out.println("application启动成功");
+    }
 }
